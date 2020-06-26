36a8202c781588f9e496b57e5fe9d0b51bff45d1
Fidel08 <64436255+Fidel08@users.noreply.github.com>
2020-06-26 21:14:07 +0000

    Add files via upload

--- Mini_Projet_Django/Retention/admin.py
+++ Mini_Projet_Django/Retention/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Patient
+from .models import Patient,Parrain
 # Register your models here.
 
 # création de la ressource d'import export
@@ -12,9 +12,20 @@ class PatientResource(resources.ModelResource):
 # Intégration simultanée dans l'espace d'administration de django de reversion et import-export
 from import_export.admin import ImportExportModelAdmin
 from reversion.admin import VersionAdmin
+
+
+
 class PatientAdmin(ImportExportModelAdmin,VersionAdmin,admin.ModelAdmin):
-    list_display=('code_patient','nom','prenom','sexe','age','numero1','numero2','date_enrol','observation') # Affichage des champs particulier lors de la modification dans l'espace d'admin (ajout supplementaire de admin.ModelAdmin dans la classe)
+    list_display=('code_patient','nom','prenom','sexe','age','cohorte_actuelle','numero1','numero2','date_enrol','observation') # Affichage des champs particulier lors de la modification dans l'espace d'admin (ajout supplementaire de admin.ModelAdmin dans la classe)
     search_fields=['code_patient','nom','prenom']
-    list_filter=('sexe','date_enrol')
+    list_filter=('sexe','date_enrol',cohorte_actuelleFilter)
     resource_class = PatientResource
+    list_per_page = 15
 admin.site.register(Patient, PatientAdmin)
+
+
+
+
+class ParrainAdmin(VersionAdmin):
+    pass
+admin.site.register(Parrain)
--- Mini_Projet_Django/Retention/models.py
+++ Mini_Projet_Django/Retention/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
 # Contraintes dans les champs du model. on définit la fonction et on l'ajoute à un champ du model via le paramètre 'validators'
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
 from string import ascii_uppercase, digits
+import re
   
 def lettres_majuscule(expression):
     for i in expression:
@@ -25,35 +27,45 @@ def numero2_8chiffres(expression):
         raise ValidationError("Le numéro de téléphone doit etre composé de 8 chiffres",params={'expression':expression},)
 
 def code_16caracteres(expression):
-    if len(expression) != 16:
-        raise ValidationError("Le code du patient doit etre composé de 16 caractères",params={'expression':expression},)
-    #elif [int(i) not in digits for i in list(expression[:4])]:
-        #raise ValidationError("Le code du centre doit etre composé uniquement de chiffres",params={'expression':expression},)
-    #elif expression[4] !="/":
-        #raise ValidationError("Le code du centre et le numéro du site doivent etre séparer par /",params={'expression':expression},)
-    #elif [int(j) not in digits for j in list(expression[5:7])]:
-        #raise ValidationError("Le numéro du site doit etre composé uniquement de chiffres",params={'expression':expression},)
-    #elif expression[7] !="/":
-        #raise ValidationError("Le numéro du site et l'année doivent etre séparer par /",params={'expression':expression},)
-    #elif [int(k) not in digits for k in list(expression[8:10])]:
-        #raise ValidationError("L'année doit etre composé uniquement de chiffres",params={'expression':expression},)
-    #elif expression[10] !="/":
-        #raise ValidationError("L'année et le numéro du patient doivent etre séparer par /",params={'expression':expression},)
-    #elif [int(l) not in digits for l in list(expression[11:])]:
-        #raise ValidationError("Le numéro du patient doit etre composé uniquement de chiffres",params={'expression':expression},)
+    if not re.match(r'^[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[0-9]{5}',expression):
+        raise ValidationError("Le format du code patient est incorrect!",params={'expression':expression},)
 
 
+class Parrain(models.Model):
+    libelle=models.CharField('Libéllé',max_length=100)
 
+    def __str__(self):
+        return self.libelle
+
+		
 class Patient(models.Model):
-    code_patient=models.CharField('Code du patient',unique=True,max_length=16,primary_key=True,validators=[code_16caracteres])
+    code_patient=models.CharField('Code du patient',unique=True,max_length=16,primary_key=True,help_text='____/__/__/_____',validators=[code_16caracteres])
     nom=models.CharField('Nom',unique=False,max_length=30,validators=[lettres_majuscule])
     prenom=models.CharField('Prénom',unique=True,max_length=100,validators=[lettres_majuscule])
     sexe=models.CharField('Sexe',max_length=10,choices=[('M','Masculin'),('F','Féminin')])
     age=models.IntegerField('Age',null=True,validators=[age_2_chiffres])
     numero1=models.IntegerField('Numéro de tel. 1',null=True,validators=[numero1_8chiffres])
     numero2=models.IntegerField('Numéro de tel. 2',blank=True,null=True, validators=[numero2_8chiffres])
-    date_enrol=models.DateField("Date d'enrollement",null=True)
+    date_enrol=models.DateField("Date d'enrollement",null=True,validators = [MaxValueValidator(datetime.datetime.now().date())])
     porte_entrer=models.CharField("Porte d'entrée",max_length=50,choices=[('PTME','PTME'),('CDV','CDV'),('MEDECINE','MEDECINE'),('INDEX TESTING','INDEX TESTING'),('MATERNITE','MATERNITE'),('COMMUNAUTE','COMMUNAUTE'),('TRANSFERT IN','TRANSFERT IN')])
-    #nom_parrain=models.ForeignKey(Parrain_marraine,on_delete=models.CASCADE)
-    parrain_marraine=models.CharField('Parrain/Marraine',max_length=50,choices=[('AHEMOU','AHEMOU'),('DIANE','DIANE'),('ELIANE','ELIANE'),('ESTELLE','ESTELLE'),('MAMBO','MAMBO'),('LARISSA','LARISSA')])
+    parrain=models.ForeignKey(Parrain,on_delete=models.SET_NULL,blank=True,null=True,related_name='patients')
     observation=models.CharField('Observation',blank=True,max_length=300,null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.nom, self.prenom)
+
+
+    @property
+    def cohorte_actuelle(self):
+        today=datetime.datetime.now()
+        diff_mois=(today.year-self.date_enrol.year) *12+today.month-self.date_enrol.month+1 # pylint: disable=no-member
+        return "M{}".format(str(diff_mois))
+
+    
+	
+        
+        
+		
+		
+		
+
